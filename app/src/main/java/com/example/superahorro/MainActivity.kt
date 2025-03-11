3c3298dcae3e793766d9525c3c6eb7f1691e375f
ispadill <ispadill@unirioja.es>
2025-03-11 18:07:33 +0100

    modifico mainActivity

--- app/src/main/java/com/example/superahorro/MainActivity.kt
+++ app/src/main/java/com/example/superahorro/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.superahorro.ui.theme.SuperAhorroTheme
 
+//ejemplo
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
